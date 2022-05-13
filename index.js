21c52e7dfd947dc54e448f4d7b4a669d13c019ed
Toluwalase Abigail <toluabby12@gmail.com>
2022-05-13 02:32:45 +0100

    Node js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const fileArr1 = require('./starter.js');
+const fileArr1 = require('./Utililities folder/utility1.1');
 console.log(`Large Numuber: ${fileArr1.largeNumber([3,5,8,10,20])}`);
 console.log(`Cut3:${fileArr1.cut3([4,6,8,9])}`);
 
 //Module.exports from module.js
-const lists = require('./module.js');
+const lists = require('./Utililities folder/utility1.2');
 lists.sum();
 lists.concat()
--- starter.js
+++ starter.js
@@ -1,19 +1,30 @@
-/*Using Exports method*/
-
-//find the largest number in an array
-exports.largeNumber = (arr) => {
+// find the largest number in an array
+const largeNumber = (arr) => {
     let largest = 0;
     arr.forEach((x) => {
         if (x > largest) {
             largest = x;
         }
-    })
+    });
     return largest;
-
-}
+};
 
 // remove the 3rd item from array
-exports.cut3 = (arr) => {
+const cut3 = (arr) => {
     arr.splice(2, 1);
-    return arr
-}
+    return arr;
+};
+
+// add numbers in an array
+const sum = (arr) => {
+    const total = 0;
+    arr.forEach((x) => {
+        total += x;
+    });
+    return total;
+};
+
+// concatenate two arrays
+const concat = (arr1, arr2) => {
+    return [...arr1, ...arr2];
+};
